f296ea6bf9a31e8efe52f3c1da6e116433d478e5
sann <psang4845@gmail.com>
2024-01-22 15:45:15 +0700

    create addressService, addressController

---
+++ src/main/java/com/base/springsecurity/repository/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.base.springsecurity.repository;
+
+import com.base.springsecurity.models.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
---
+++ src/main/java/com/base/springsecurity/services/serviceImpl/AddressServiceImpl.java
@@ -0,0 +1,74 @@
+package com.base.springsecurity.services.serviceImpl;
+
+import com.base.springsecurity.exceptions.ResourceNotFoundException;
+import com.base.springsecurity.models.dto.catalog.address.AddressDTO;
+import com.base.springsecurity.models.entity.Address;
+import com.base.springsecurity.repository.AddressRepository;
+import com.base.springsecurity.services.AddressService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+
+    @Autowired
+    private AddressRepository addressRepository;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public List<AddressDTO> getAllAddresses() {
+        //DTO -> Entity
+        List<Address> listAddressEntity = addressRepository.findAll();
+        //Entity -> DTO
+        return listAddressEntity.stream()
+            .map((address) -> modelMapper.map(address, AddressDTO.class))
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    public AddressDTO getAddressById(Long id) {
+        Address address = addressRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Address", "id", id));
+        return  modelMapper.map(address, AddressDTO.class);
+    }
+
+    @Override
+    public AddressDTO insertAddress(AddressDTO addressDTO) {
+        //Map DTO -> Entity
+        Address address = modelMapper.map(addressDTO, Address.class);
+        //Save Address
+        Address savedAddress = addressRepository.save(address);
+        //Map entity -> DTO
+        return modelMapper.map(savedAddress, AddressDTO.class);
+    }
+
+    @Override
+    public AddressDTO updateAddress(AddressDTO addressDTO, Long id) {
+        Address address = addressRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Address", "id", id));
+        //Map DTO -> Entity
+        address.setFirstName(addressDTO.getFirstName());
+        address.setLastName(addressDTO.getLastName());
+        address.setStreetAddress(addressDTO.getStreetAddress());
+        address.setState(addressDTO.getState());
+        address.setZipCode(addressDTO.getZipCode());
+        address.setCity(addressDTO.getCity());
+        address.setMobile(addressDTO.getMobile());
+
+        Address updateAddress = addressRepository.save(address);
+        //Map Entity -> DTO
+        return  modelMapper.map(updateAddress, AddressDTO.class);
+    }
+
+    @Override
+    public void deleteAddress(Long id) {
+        Address address = addressRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Address", "id", id));
+        addressRepository.delete(address);
+    }
+}
