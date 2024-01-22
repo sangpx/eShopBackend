89b048a09edeb9a420f631a718fd2f235d7a215a
sann <psang4845@gmail.com>
2024-01-22 17:30:58 +0700

    fileService, fileController

--- src/main/java/com/base/springsecurity/controllers/RoleController.java
+++ src/main/java/com/base/springsecurity/controllers/RoleController.java
@@ -14,14 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/role")
+@RequestMapping("/api/roles")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class RoleController {
     @Autowired
     private RoleService roleService;
 
-    @GetMapping("/getRoles")
+    @GetMapping("/getAllRoles")
     public ResponseEntity<?> getAllRoles() {
         List<Role> roles = roleService.getAllRoles();
         return ResponseEntity.ok(roles);
