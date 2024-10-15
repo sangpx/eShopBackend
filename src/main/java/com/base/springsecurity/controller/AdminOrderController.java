68f05cd13ff1745a5ce3bd52ca360da4edb74518
sangpx <psang4845@gmail.com>
2024-10-15 23:58:15 +0700

    update ti

--- src/main/java/com/base/springsecurity/controller/AdminOrderController.java
+++ src/main/java/com/base/springsecurity/controller/AdminOrderController.java
@@ -27,31 +27,31 @@ public class AdminOrderController {
 
     @PutMapping("/{orderId}/confirmed")
     public ResponseEntity<Order> ConfirmedOrderHandler(@PathVariable Long orderId) throws OrderException {
-        Order order=orderService.confirmedOrder(orderId);
+        Order order = orderService.confirmedOrder(orderId);
         return new ResponseEntity<Order>(order,HttpStatus.OK);
     }
 
     @PutMapping("/{orderId}/ship")
     public ResponseEntity<Order> shippedOrderHandler(@PathVariable Long orderId) throws OrderException{
-        Order order=orderService.shippedOrder(orderId);
+        Order order = orderService.shippedOrder(orderId);
         return new ResponseEntity<Order>(order, HttpStatus.OK);
     }
 
     @PutMapping("/{orderId}/deliver")
     public ResponseEntity<Order> deliveredOrderHandler(@PathVariable Long orderId) throws OrderException{
-        Order order=orderService.deliveredOrder(orderId);
+        Order order = orderService.deliveredOrder(orderId);
         return new ResponseEntity<Order>(order, HttpStatus.OK);
     }
 
     @PutMapping("/{orderId}/cancel")
     public ResponseEntity<Order> canceledOrderHandler(@PathVariable Long orderId) throws OrderException{
-        Order order=orderService.cancledOrder(orderId);
+        Order order = orderService.cancledOrder(orderId);
         return new ResponseEntity<Order>(order, HttpStatus.OK);
     }
 
     @PutMapping("/{orderId}/placed")
     public ResponseEntity<Order> placedOrderHandler(@PathVariable Long orderId) throws OrderException{
-        Order order=orderService.placedOrder(orderId);
+        Order order = orderService.placedOrder(orderId);
         return new ResponseEntity<Order>(order, HttpStatus.OK);
     }
 
